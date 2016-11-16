c32e8637e010274bcec5a2687b816bae953de822
Vikas Vellanki <vikas.vellanki@outlook.com>
2016-11-15 20:08:19 -0500

    Completed Search UI

--- ReadMe.txt
+++ ReadMe.txt
@@ -2,4 +2,4 @@ Two steps to run the page:
 	- Open index.html in any one of the web browsers. 
 	- Enter artist name in the search box and click search or press Enter.
 
-Validaret for empty and invalid artist names.
+Validated for empty and invalid artist names.
--- app.js
+++ app.js
@@ -1,16 +1,12 @@
 (function(){
-
   'use strict'
-
   $(document).ready(function(){
-
-    // For Enter keyword
+    // Enter keyword Functaionality 
     $("#inputid").keypress(function (e) {
-      if (e.which == 13) {
+      if (e.which === 13) {
            e.preventDefault();
            var id = $("#inputid").val();
-
-       // validating for empty artist name entries
+         // validating artist name when empty
          if(id.length === 0){
             $("#added").empty();
             $(".searchDiv").addClass("vertical");
@@ -18,7 +14,7 @@
             $(".message").text("Please enter an valid artist name");
             return;
          }
-        
+         // Fetching artists through api call
          var type = "artist";
          $.ajax({
             url:"https://api.spotify.com/v1/search?q=" + id + "&type=" + type,
@@ -31,8 +27,7 @@
     // click functionality
     $("#searchArtist").click(function(){
        var id = $("#inputid").val();
-
-       // validating for empty artist name entries
+       // validating artist name when empty
        if(id.length === 0){
           $("#added").empty();
           $(".searchDiv").addClass("vertical");
@@ -40,7 +35,7 @@
           $(".message").text("Please enter an valid artist name");
           return;
        }
-      
+       // Fetching artists through api call
        var type = "artist";
        $.ajax({
           url:"https://api.spotify.com/v1/search?q=" + id + "&type=" + type,
@@ -53,30 +48,33 @@
 
 function artistResults(results){ 
   var artists = results.artists;
-  var items = artists.items.splice(0,5);
-
-  //reset messages and the list of artists
-  $("#added").empty();
-  $(".message").text("");
-
-  $.each(items, function(index,item){
-        displayArtist(item);
-  })
-
-  if(items.length>0){ //if artists found
-    $(".searchDiv").removeClass("vertical");
-    $("#added").addClass("displayDiv");
-  }else{ //if there are no results returned
+  // When wrong artist name is given as input, error message is displayed
+  if(artists.total === 0){
+    $("#added").empty();
     $(".searchDiv").addClass("vertical");
     $("#added").removeClass("displayDiv");
     $(".message").text("No artist exists with this name");
+    return;
+  }
+  else{
+    // Displaying artist info when artist name is given
+    var items = artists.items.splice(0,6);
+    //reset messages and the list of artists
+    $("#added").empty();
+    $(".message").text("");
+    $.each(items, function(index,item){
+          displayArtist(item);
+    })
+    $(".searchDiv").removeClass("vertical");
+    $("#added").addClass("displayDiv");
   }
 };
 
 function displayArtist(item){
-     var image = (item.images.length > 0) ? item.images[0].url : "images\\Placeholder_person.png";
-     $("#added").append("");
-     $("#added").append("<div class='col-xs-4 center-block thumb'><img src='" + image + "' class='img-responsive img-rounded'/><h4 class='add-artist'>"+item.name+"</h4></div>");
-     $("#added").append("");
+  // Artists fetch results(names and images) are displayed
+  var image = (item.images.length > 0) ? item.images[0].url : "images\\Placeholder_person.png";
+  $("#added").append("");
+  $("#added").append("<div class='col-xs-4 center-block thumb mymargin'><img src='" + image + "' class='img-responsive img-rounded'/><h4 class='add-artist'>"+item.name+"</h4></div>");
+  $("#added").append("");
 }
 })();
